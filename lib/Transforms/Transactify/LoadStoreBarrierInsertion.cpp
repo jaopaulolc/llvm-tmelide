d5dcc7ebe2603372d69d5add304e295f8d60ca7b
João P. L. de Carvalho <jaopaulolc@gmail.com>
2018-08-24 13:04:19 -0300

    Add support for some vector types

--- lib/Transforms/Transactify/LoadStoreBarrierInsertion.cpp
+++ lib/Transforms/Transactify/LoadStoreBarrierInsertion.cpp
@@ -8,6 +8,7 @@
 #include "llvm/IR/Attributes.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/TypeBuilder.h"
+#include "llvm/IR/DerivedTypes.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/Transforms/Utils/Cloning.h"
 #include "llvm/Transforms/Utils/ValueMapper.h"
@@ -65,6 +66,14 @@ struct LoadStoreBarriers {
   LOAD_FUNC_DECL(F)
   LOAD_FUNC_DECL(D)
 //  LOAD_FUNC_DECL(E)
+  LOAD_FUNC_DECL(M128i)
+  LOAD_FUNC_DECL(M128ii)
+  LOAD_FUNC_DECL(M128)
+  LOAD_FUNC_DECL(M128d)
+  LOAD_FUNC_DECL(M256i)
+  LOAD_FUNC_DECL(M256ii)
+  LOAD_FUNC_DECL(M256)
+  LOAD_FUNC_DECL(M256d)
 #undef LOAD_FUNC_DECL
 
 #define STORE_FUNC_DECL(SUFFIX) \
@@ -77,6 +86,14 @@ struct LoadStoreBarriers {
   STORE_FUNC_DECL(F)
   STORE_FUNC_DECL(D)
 //  STORE_FUNC_DECL(E)
+  STORE_FUNC_DECL(M128i)
+  STORE_FUNC_DECL(M128ii)
+  STORE_FUNC_DECL(M128)
+  STORE_FUNC_DECL(M128d)
+  STORE_FUNC_DECL(M256i)
+  STORE_FUNC_DECL(M256ii)
+  STORE_FUNC_DECL(M256)
+  STORE_FUNC_DECL(M256d)
 #undef STORE_FUNC_DECL
 
 #define ADD_LOAD_DEF(TYPE, SUFFIX, NAME) \
@@ -103,6 +120,32 @@ struct LoadStoreBarriers {
     } \
   }
 
+#define ADD_LOAD_VECTOR_DEF(TYPE, SIZE, SUFFIX, NAME) \
+  Function* addDefinitionTo##SUFFIX() { \
+    LLVMContext& context = TheModule.getContext(); \
+    StringRef functionName = StringRef(NAME); \
+    VectorType* VT = VectorType::get(Type::get##TYPE(context), SIZE); \
+    SmallVector<Type*, 1> Params; \
+    Params.push_back(VT->getPointerTo()); \
+    FunctionType *functionType = FunctionType::get(VT, Params, false); \
+    AttributeList attrList = \
+      AttributeList() \
+        .addAttribute(context, \
+            AttributeList::FunctionIndex, Attribute::NoInline); \
+    Constant *ret = TheModule.getOrInsertFunction( \
+        functionName, functionType, attrList); \
+    if (isa<Function>(ret)) { \
+      /*errs() <<  "successfully inserted "; \
+      errs() << functionName << " definition!" << '\n'; \
+      static_cast<Function*>(ret)->print(llvm::errs()); */ \
+      return static_cast<Function*>(ret); \
+    } else { \
+      errs() <<  "failed to insert "; \
+      errs() << functionName << " definition!" << '\n'; \
+      return nullptr; \
+    } \
+  }
+
   ADD_LOAD_DEF(uint8_t    , RU1, "_ITM_RU1")
   ADD_LOAD_DEF(uint16_t   , RU2, "_ITM_RU2")
   ADD_LOAD_DEF(uint32_t   , RU4, "_ITM_RU4")
@@ -110,6 +153,14 @@ struct LoadStoreBarriers {
   ADD_LOAD_DEF(float      , RF , "_ITM_RF")
   ADD_LOAD_DEF(double     , RD , "_ITM_RD")
 //  ADD_LOAD_DEF(long double, RE , "_ITM_RE")
+  ADD_LOAD_VECTOR_DEF(Int32Ty , /* <4 x i32>    */4, RM128i , "_ITM_RM128i")
+  ADD_LOAD_VECTOR_DEF(Int64Ty , /* <2 x i64>    */2, RM128ii, "_ITM_RM128ii")
+  ADD_LOAD_VECTOR_DEF(FloatTy , /* <4 x float>  */4, RM128  , "_ITM_RM128")
+  ADD_LOAD_VECTOR_DEF(DoubleTy, /* <2 x double> */2, RM128d , "_ITM_RM128d")
+  ADD_LOAD_VECTOR_DEF(Int32Ty , /* <8 x i32>    */8, RM256i , "_ITM_RM256i")
+  ADD_LOAD_VECTOR_DEF(Int64Ty , /* <4 x i64>    */4, RM256ii, "_ITM_RM256ii")
+  ADD_LOAD_VECTOR_DEF(FloatTy , /* <8 x float>  */8, RM256  , "_ITM_RM256")
+  ADD_LOAD_VECTOR_DEF(DoubleTy, /* <2 x double> */4, RM256d , "_ITM_RM256d")
 #undef ADD_LOAD_DEF
 
 #define ADD_STORE_DEF(TYPE, SUFFIX, NAME) \
@@ -136,6 +187,34 @@ struct LoadStoreBarriers {
     } \
   }
 
+#define ADD_STORE_VECTOR_DEF(TYPE, SIZE, SUFFIX, NAME) \
+  Function* addDefinitionTo##SUFFIX() { \
+    LLVMContext& context = TheModule.getContext(); \
+    StringRef functionName = StringRef(NAME); \
+    VectorType* VT = VectorType::get(Type::get##TYPE(context), SIZE); \
+    SmallVector<Type*, 2> Params; \
+    Params.push_back(VT->getPointerTo()); \
+    Params.push_back(VT); \
+    FunctionType *functionType = \
+      FunctionType::get(Type::getVoidTy(context), Params, false); \
+    AttributeList attrList = \
+      AttributeList() \
+        .addAttribute(context, \
+            AttributeList::FunctionIndex, Attribute::NoInline); \
+    Constant *ret = TheModule.getOrInsertFunction( \
+        functionName, functionType, attrList); \
+    if (isa<Function>(ret)) { \
+      /*errs() <<  "successfully inserted "; \
+      errs() << functionName << " definition!" << '\n'; \
+      static_cast<Function*>(ret)->print(llvm::errs()); */ \
+      return static_cast<Function*>(ret); \
+    } else { \
+      errs() <<  "failed to insert "; \
+      errs() << functionName << " definition!" << '\n'; \
+      return nullptr; \
+    } \
+  }
+
   ADD_STORE_DEF(uint8_t    , WU1, "_ITM_WU1")
   ADD_STORE_DEF(uint16_t   , WU2, "_ITM_WU2")
   ADD_STORE_DEF(uint32_t   , WU4, "_ITM_WU4")
@@ -143,6 +222,14 @@ struct LoadStoreBarriers {
   ADD_STORE_DEF(float      , WF , "_ITM_WF")
   ADD_STORE_DEF(double     , WD , "_ITM_WD")
 //  ADD_STORE_DEF(long double, WE , "_ITM_WE")
+  ADD_STORE_VECTOR_DEF(Int32Ty , /* <4 x i32>    */4, WM128i , "_ITM_WM128i")
+  ADD_STORE_VECTOR_DEF(Int64Ty , /* <2 x i64>    */2, WM128ii, "_ITM_WM128ii")
+  ADD_STORE_VECTOR_DEF(FloatTy , /* <4 x float>  */4, WM128  , "_ITM_WM128")
+  ADD_STORE_VECTOR_DEF(DoubleTy, /* <2 x double> */2, WM128d , "_ITM_WM128d")
+  ADD_STORE_VECTOR_DEF(Int32Ty , /* <8 x i32>    */8, WM256i , "_ITM_WM256i")
+  ADD_STORE_VECTOR_DEF(Int64Ty , /* <4 x i64>    */4, WM256ii, "_ITM_WM256ii")
+  ADD_STORE_VECTOR_DEF(FloatTy , /* <8 x float>  */8, WM256  , "_ITM_WM256")
+  ADD_STORE_VECTOR_DEF(DoubleTy, /* <2 x double> */4, WM256d , "_ITM_WM256d")
 #undef ADD_STORE_DEF
 
 #define INIT_LOAD_FUNC_FIELD(SUFFIX) \
@@ -159,6 +246,14 @@ public:
     INIT_LOAD_FUNC_FIELD(F)
     INIT_LOAD_FUNC_FIELD(D)
 //    INIT_LOAD_FUNC_FIELD(E)
+    INIT_LOAD_FUNC_FIELD(M128i)
+    INIT_LOAD_FUNC_FIELD(M128ii)
+    INIT_LOAD_FUNC_FIELD(M128)
+    INIT_LOAD_FUNC_FIELD(M128d)
+    INIT_LOAD_FUNC_FIELD(M256i)
+    INIT_LOAD_FUNC_FIELD(M256ii)
+    INIT_LOAD_FUNC_FIELD(M256)
+    INIT_LOAD_FUNC_FIELD(M256d)
 #undef INIT_LOAD_FUNC_FIELD
     INIT_STORE_FUNC_FIELD(U1)
     INIT_STORE_FUNC_FIELD(U2)
@@ -167,6 +262,14 @@ public:
     INIT_STORE_FUNC_FIELD(F)
     INIT_STORE_FUNC_FIELD(D)
 //    INIT_STORE_FUNC_FIELD(E)
+    INIT_STORE_FUNC_FIELD(M128i)
+    INIT_STORE_FUNC_FIELD(M128ii)
+    INIT_STORE_FUNC_FIELD(M128)
+    INIT_STORE_FUNC_FIELD(M128d)
+    INIT_STORE_FUNC_FIELD(M256i)
+    INIT_STORE_FUNC_FIELD(M256ii)
+    INIT_STORE_FUNC_FIELD(M256)
+    INIT_STORE_FUNC_FIELD(M256d)
 #undef INIT_STORE_FUNC_FIELD
   }
 
@@ -182,6 +285,14 @@ public:
   LOAD_GETTER(F)
   LOAD_GETTER(D)
 //  LOAD_GETTER(E)
+  LOAD_GETTER(M128i)
+  LOAD_GETTER(M128ii)
+  LOAD_GETTER(M128)
+  LOAD_GETTER(M128d)
+  LOAD_GETTER(M256i)
+  LOAD_GETTER(M256ii)
+  LOAD_GETTER(M256)
+  LOAD_GETTER(M256d)
 #undef LOAD_GETTER
 
 #define STORE_GETTER(SUFFIX) \
@@ -196,6 +307,14 @@ public:
   STORE_GETTER(F)
   STORE_GETTER(D)
 //  STORE_GETTER(E)
+  STORE_GETTER(M128i)
+  STORE_GETTER(M128ii)
+  STORE_GETTER(M128)
+  STORE_GETTER(M128d)
+  STORE_GETTER(M256i)
+  STORE_GETTER(M256ii)
+  STORE_GETTER(M256)
+  STORE_GETTER(M256d)
 #undef STORE_GETTER
 };
 
@@ -213,6 +332,14 @@ struct LogBarriers {
   LOG_FUNC_DECL(F)
   LOG_FUNC_DECL(D)
 //  LOG_FUNC_DECL(E)
+  LOG_FUNC_DECL(M128i)
+  LOG_FUNC_DECL(M128ii)
+  LOG_FUNC_DECL(M128)
+  LOG_FUNC_DECL(M128d)
+  LOG_FUNC_DECL(M256i)
+  LOG_FUNC_DECL(M256ii)
+  LOG_FUNC_DECL(M256)
+  LOG_FUNC_DECL(M256d)
 #undef LOG_FUNC_DECL
   Function* LogB;
 
@@ -239,6 +366,34 @@ struct LogBarriers {
       return NULL; \
     } \
   }
+
+#define ADD_LOG_VECTOR_DEF(TYPE, SIZE, SUFFIX, NAME) \
+  Function* addDefinitionTo##SUFFIX() { \
+    LLVMContext& context = TheModule.getContext(); \
+    StringRef functionName = StringRef(NAME); \
+    VectorType* VT = VectorType::get(Type::get##TYPE(context), SIZE); \
+    SmallVector<Type*, 1> Params; \
+    Params.push_back(VT->getPointerTo()); \
+    FunctionType *functionType = \
+      FunctionType::get(Type::getVoidTy(context), Params, false); \
+    AttributeList attrList = \
+      AttributeList() \
+        .addAttribute(context, \
+            AttributeList::FunctionIndex, Attribute::NoInline); \
+    Constant *ret = TheModule.getOrInsertFunction( \
+        functionName, functionType, attrList); \
+    if (isa<Function>(ret)) { \
+      /*errs() <<  "successfully inserted "; \
+      errs() << functionName << " definition!" << '\n'; \
+      static_cast<Function*>(ret)->print(llvm::errs()); */ \
+      return static_cast<Function*>(ret); \
+    } else { \
+      errs() <<  "failed to insert "; \
+      errs() << functionName << " definition!" << '\n'; \
+      return nullptr; \
+    } \
+  }
+
 
   Function* addDefinitionToLB() {
     LLVMContext& context = TheModule.getContext();
@@ -270,7 +425,16 @@ struct LogBarriers {
   ADD_LOG_DEF(float      , LF , "_ITM_LF")
   ADD_LOG_DEF(double     , LD , "_ITM_LD")
 //  ADD_LOG_DEF(long double, LE , "_ITM_LE")
+  ADD_LOG_VECTOR_DEF(Int32Ty , /* <4 x i32>    */4, LM128i , "_ITM_LM128i")
+  ADD_LOG_VECTOR_DEF(Int64Ty , /* <2 x i64>    */2, LM128ii, "_ITM_LM128ii")
+  ADD_LOG_VECTOR_DEF(FloatTy , /* <4 x float>  */4, LM128  , "_ITM_LM128")
+  ADD_LOG_VECTOR_DEF(DoubleTy, /* <2 x double> */2, LM128d , "_ITM_LM128d")
+  ADD_LOG_VECTOR_DEF(Int32Ty , /* <8 x i32>    */8, LM256i , "_ITM_LM256i")
+  ADD_LOG_VECTOR_DEF(Int64Ty , /* <4 x i64>    */4, LM256ii, "_ITM_LM256ii")
+  ADD_LOG_VECTOR_DEF(FloatTy , /* <8 x float>  */8, LM256  , "_ITM_LM256")
+  ADD_LOG_VECTOR_DEF(DoubleTy, /* <2 x double> */4, LM256d , "_ITM_LM256d")
 #undef ADD_LOG_DEF
+#undef ADD_LOG_VECTOR_DEF
 
 #define INIT_LOG_FUNC_FIELD(SUFFIX) \
   Log##SUFFIX = addDefinitionToL##SUFFIX();
@@ -283,8 +447,16 @@ public:
     INIT_LOG_FUNC_FIELD(U8)
     INIT_LOG_FUNC_FIELD(F)
     INIT_LOG_FUNC_FIELD(D)
-    LogB = addDefinitionToLB();
 //    INIT_LOG_FUNC_FIELD(E)
+    INIT_LOG_FUNC_FIELD(M128i)
+    INIT_LOG_FUNC_FIELD(M128ii)
+    INIT_LOG_FUNC_FIELD(M128)
+    INIT_LOG_FUNC_FIELD(M128d)
+    INIT_LOG_FUNC_FIELD(M256i)
+    INIT_LOG_FUNC_FIELD(M256ii)
+    INIT_LOG_FUNC_FIELD(M256)
+    INIT_LOG_FUNC_FIELD(M256d)
+    LogB = addDefinitionToLB();
 #undef INIT_LOG_FUNC_FIELD
   }
 
@@ -300,6 +472,14 @@ public:
   LOG_GETTER(F)
   LOG_GETTER(D)
 //  LOG_GETTER(E)
+  LOG_GETTER(M128i)
+  LOG_GETTER(M128ii)
+  LOG_GETTER(M128)
+  LOG_GETTER(M128d)
+  LOG_GETTER(M256i)
+  LOG_GETTER(M256ii)
+  LOG_GETTER(M256)
+  LOG_GETTER(M256d)
 #undef LOG_GETTER
   Function* getLogB() {
     return LogB;
@@ -333,9 +513,72 @@ static void insertLoadBarrier(LoadStoreBarriers &LSBarriers,
   std::vector<Value*> Args(1);
   unsigned TypeSize = LoadType->getPrimitiveSizeInBits();
   Args[0] = Load.getPointerOperand();
-  if (LoadType->isIntegerTy() ||
-      (TypeSize &&
-       !LoadType->isFloatTy() && !LoadType->isDoubleTy()) ) {
+  if (LoadType->isX86_MMXTy()) {
+    errs() << "MMX/AVX instruction FOUND!!!!!\n";
+  } else if (LoadType->isVectorTy()) {
+    Type* ElementType =  LoadType->getVectorElementType();
+    switch(TypeSize) {
+      case 128:
+        if (isa<IntegerType>(ElementType)) {
+          unsigned ElementTypeSize = ElementType->getPrimitiveSizeInBits();
+          switch(ElementTypeSize) {
+            case 32:
+                Callee = LSBarriers.getLoadM128i();
+              break;
+            case 64:
+                Callee = LSBarriers.getLoadM128ii();
+              break;
+            default:
+              errs() << "unsupported integer size for vector element \n";
+              Load.print(errs(), true);
+              errs() << '\n';
+              break;
+          }
+        } else if (ElementType->isFloatTy()) {
+          Callee = LSBarriers.getLoadM128();
+        } else if (ElementType->isDoubleTy()) {
+          Callee = LSBarriers.getLoadM128d();
+        } else {
+          errs() << "unsupported vector type \n";
+          Load.print(errs(), true);
+          errs() << '\n';
+        }
+        break;
+      case 256:
+        if (isa<IntegerType>(ElementType)) {
+          unsigned ElementTypeSize = ElementType->getPrimitiveSizeInBits();
+          switch(ElementTypeSize) {
+            case 32:
+                Callee = LSBarriers.getLoadM256i();
+              break;
+            case 64:
+                Callee = LSBarriers.getLoadM256ii();
+              break;
+            default:
+              errs() << "unsupported integer size for vector element \n";
+              Load.print(errs(), true);
+              errs() << '\n';
+              break;
+          }
+        } else if (ElementType->isFloatTy()) {
+          Callee = LSBarriers.getLoadM256();
+        } else if (ElementType->isDoubleTy()) {
+          Callee = LSBarriers.getLoadM256d();
+        } else {
+          errs() << "unsupported vector type \n";
+          Load.print(errs(), true);
+          errs() << '\n';
+        }
+        break;
+      default:
+        errs() << "unsupported vector size '" << TypeSize << "'-bit\n";
+        Load.print(errs(), true);
+        errs() << '\n';
+        break;
+    }
+  } else if (LoadType->isArrayTy()) {
+    errs() << "ARRAY TYPE\n";
+  } else if (LoadType->isIntegerTy()) {
     switch(TypeSize) {
       case 8:
         Callee = LSBarriers.getLoadU1();
@@ -388,9 +631,72 @@ static void insertStoreBarrier(LoadStoreBarriers &LSBarriers,
   Args[0] = Store.getPointerOperand();
   Args[1] = Store.getValueOperand();
   unsigned TypeSize = StoreType->getPrimitiveSizeInBits();
-  if (StoreType->isIntegerTy() ||
-      (TypeSize &&
-       !StoreType->isFloatTy() && !StoreType->isDoubleTy()) ) {
+  if (StoreType->isX86_MMXTy()) {
+    errs() << "MMX/AVX instruction FOUND!!!!!\n";
+  } else if (StoreType->isVectorTy()) {
+    Type* ElementType =  StoreType->getVectorElementType();
+    switch(TypeSize) {
+      case 128:
+        if (isa<IntegerType>(ElementType)) {
+          unsigned ElementTypeSize = ElementType->getPrimitiveSizeInBits();
+          switch(ElementTypeSize) {
+            case 32:
+                Callee = LSBarriers.getStoreM128i();
+              break;
+            case 64:
+                Callee = LSBarriers.getStoreM128ii();
+              break;
+            default:
+              errs() << "unsupported integer size for vector element \n";
+              Store.print(errs(), true);
+              errs() << '\n';
+              break;
+          }
+        } else if (ElementType->isFloatTy()) {
+          Callee = LSBarriers.getStoreM128();
+        } else if (ElementType->isDoubleTy()) {
+          Callee = LSBarriers.getStoreM128d();
+        } else {
+          errs() << "unsupported vector type \n";
+          Store.print(errs(), true);
+          errs() << '\n';
+        }
+        break;
+      case 256:
+        if (isa<IntegerType>(ElementType)) {
+          unsigned ElementTypeSize = ElementType->getPrimitiveSizeInBits();
+          switch(ElementTypeSize) {
+            case 32:
+                Callee = LSBarriers.getStoreM256i();
+              break;
+            case 64:
+                Callee = LSBarriers.getStoreM256ii();
+              break;
+            default:
+              errs() << "unsupported integer size for vector element \n";
+              Store.print(errs(), true);
+              errs() << '\n';
+              break;
+          }
+        } else if (ElementType->isFloatTy()) {
+          Callee = LSBarriers.getStoreM256();
+        } else if (ElementType->isDoubleTy()) {
+          Callee = LSBarriers.getStoreM256d();
+        } else {
+          errs() << "unsupported vector type \n";
+          Store.print(errs(), true);
+          errs() << '\n';
+        }
+        break;
+      default:
+        errs() << "unsupported vector size '" << TypeSize << "'-bit\n";
+        Store.print(errs(), true);
+        errs() << '\n';
+        break;
+    }
+  } else if (StoreType->isArrayTy()) {
+    errs() << "ARRAY TYPE\n";
+  } else if (StoreType->isIntegerTy()) {
     switch(TypeSize) {
       case 8:
         Callee = LSBarriers.getStoreU1();
@@ -440,9 +746,72 @@ static void insertLogBarrier(LogBarriers &LBarriers, Instruction &I) {
   std::vector<Value*> Args(1);
   Args[0] = Store.getPointerOperand();
   unsigned TypeSize = StoreType->getPrimitiveSizeInBits();
-  if (StoreType->isIntegerTy() ||
-      (TypeSize &&
-       !StoreType->isFloatTy() && !StoreType->isDoubleTy()) ) {
+  if (StoreType->isX86_MMXTy()) {
+    errs() << "MMX/AVX instruction FOUND!!!!!\n";
+  } else if (StoreType->isVectorTy()) {
+    Type* ElementType =  StoreType->getVectorElementType();
+    switch(TypeSize) {
+      case 128:
+        if (isa<IntegerType>(ElementType)) {
+          unsigned ElementTypeSize = ElementType->getPrimitiveSizeInBits();
+          switch(ElementTypeSize) {
+            case 32:
+                Callee = LBarriers.getLogM128i();
+              break;
+            case 64:
+                Callee = LBarriers.getLogM128ii();
+              break;
+            default:
+              errs() << "unsupported integer size for vector element \n";
+              Store.print(errs(), true);
+              errs() << '\n';
+              break;
+          }
+        } else if (ElementType->isFloatTy()) {
+          Callee = LBarriers.getLogM128();
+        } else if (ElementType->isDoubleTy()) {
+          Callee = LBarriers.getLogM128d();
+        } else {
+          errs() << "unsupported vector type \n";
+          Store.print(errs(), true);
+          errs() << '\n';
+        }
+        break;
+      case 256:
+        if (isa<IntegerType>(ElementType)) {
+          unsigned ElementTypeSize = ElementType->getPrimitiveSizeInBits();
+          switch(ElementTypeSize) {
+            case 32:
+                Callee = LBarriers.getLogM256i();
+              break;
+            case 64:
+                Callee = LBarriers.getLogM256ii();
+              break;
+            default:
+              errs() << "unsupported integer size for vector element \n";
+              Store.print(errs(), true);
+              errs() << '\n';
+              break;
+          }
+        } else if (ElementType->isFloatTy()) {
+          Callee = LBarriers.getLogM256();
+        } else if (ElementType->isDoubleTy()) {
+          Callee = LBarriers.getLogM256d();
+        } else {
+          errs() << "unsupported vector type \n";
+          Store.print(errs(), true);
+          errs() << '\n';
+        }
+        break;
+      default:
+        errs() << "unsupported vector size '" << TypeSize << "'-bit\n";
+        Store.print(errs(), true);
+        errs() << '\n';
+        break;
+    }
+  } else if (StoreType->isArrayTy()) {
+    errs() << "ARRAY TYPE\n";
+  } else if (StoreType->isIntegerTy()) {
     switch(TypeSize) {
       case 8:
         Callee = LBarriers.getLogU1();
