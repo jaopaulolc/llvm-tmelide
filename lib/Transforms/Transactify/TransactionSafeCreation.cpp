7f9a902bac456255848fe3271930b760c50f5c06
João P. L. de Carvalho <jaopaulolc@gmail.com>
2018-09-14 15:09:56 -0300

    Create __TMC_LIST__  and __TMC_END__ in .tm_clone_table section  - __TMC_LIST__ is a plain list of orig,clone function pointers  - __TMC_END__ is a hidden symbol which marks the end of __TMC_LIST__

--- lib/Transforms/Transactify/TransactionSafeCreation.cpp
+++ lib/Transforms/Transactify/TransactionSafeCreation.cpp
@@ -1,5 +1,7 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/TypeBuilder.h"
 #include "llvm/Transforms/Utils/Cloning.h"
 #include "llvm/Transforms/Utils/ValueMapper.h"
 #include "llvm/Support/Casting.h"
@@ -7,6 +9,9 @@
 
 #include "llvm/Analysis/TransactionAtomicInfo.h"
 #include "llvm/Transforms/Transactify/TransactionSafeCreation.h"
+
+#include <unordered_set>
+#include <utility>
 
 using namespace llvm;
 
@@ -48,14 +53,45 @@ ModulePass* createTransactionSafeCreationPass() {
 
 } // end namespace llvm
 
-bool TransactionSafeCreationPass::runImpl(Function &F) {
+namespace {
 
-  if (!F.hasFnAttribute(Attribute::AttrKind::TransactionSafe)) {
-    return false;
+void registerTransactionClones(Module &TheModule,
+    std::unordered_set<std::pair<Function*, Function*>*>& Clones) {
+
+  LLVMContext &C = TheModule.getContext();
+  PointerType* VoidPtrTy =
+    PointerType::get(TypeBuilder<void(void), false>::get(C), /*AS*/0);
+  ArrayType* VoidPtrArrayTy = ArrayType::get(VoidPtrTy, 2*Clones.size());
+  SmallVector<Constant*, 8> Pointers;
+  for (std::pair<Function*,Function*>* p : Clones) {
+    errs() << "created: " << p->first << ", " << p->second << "" << '\n';
+    Pointers.push_back(p->first);
+    Pointers.push_back(p->second);
   }
+  Constant* initializer = ConstantArray::get(VoidPtrArrayTy, Pointers);
+  initializer->print(errs(), true);
+  GlobalVariable *TMC_LIST = new GlobalVariable(TheModule, VoidPtrArrayTy,
+      /*isConstant*/true, GlobalVariable::LinkageTypes::LinkOnceAnyLinkage,
+      nullptr);
+  TMC_LIST->setName("__TMC_LIST__");
+  TMC_LIST->setInitializer(initializer);
+  TMC_LIST->setSection(".tm_clone_table");
+  TMC_LIST->setAlignment(sizeof(void*));
+
+  GlobalVariable *TMC_END = new GlobalVariable(TheModule,
+      ArrayType::get(VoidPtrTy, 2), /*isConstant*/true,
+      GlobalVariable::LinkageTypes::LinkOnceAnyLinkage, nullptr);
+  TMC_END->setName("__TMC_END__");
+  TMC_END->setAlignment(sizeof(void*));
+  TMC_END->setSection(".tm_clone_table");
+  TMC_END->setVisibility(GlobalVariable::VisibilityTypes::HiddenVisibility);
+}
+
+} // end anonymous namespace
 
 bool TransactionSafeCreationPass::runImpl(Module &M) {
 
+  std::unordered_set<std::pair<Function*, Function*>*> tm_clones;
   for (Function &F : M.getFunctionList()) {
 
     if (F.empty()) continue;
@@ -72,8 +108,15 @@ bool TransactionSafeCreationPass::runImpl(Module &M) {
     Twine cloneName = "__transactional_clone." + Twine(F.getName());
     TxSafeClone->setName(cloneName);
     TxSafeClone->removeFnAttr(Attribute::AttrKind::TransactionSafe);
+    tm_clones.insert(new std::pair<Function*,Function*>(&F, TxSafeClone));
   }
-  return true;
+
+  if (tm_clones.empty()) {
+    return false;
+  } else {
+    registerTransactionClones(M, tm_clones);
+    return true;
+  }
 }
 
 PreservedAnalyses TransactionSafeCreationPass::run(Module &M,
